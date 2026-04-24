9f810acc625f8e3355caccc11b4c398212ada879
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-04-24 09:26:38 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
 
-int my_strcmp(const char *a, const char *b) {
-
-}
-
-int main() {
-
-  printf("%d\n", my_strcmp("abc", "abcd"));
-  printf("%d\n", my_strcmp("abc", "abc"));
-  printf("%d\n", my_strcmp("b", "a"));
-
-  return 0;
-}
