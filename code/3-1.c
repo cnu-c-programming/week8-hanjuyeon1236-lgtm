e7c31827f30a56008f957c81f474fb4788c02390
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-04-30 21:08:17 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -3,7 +3,7 @@
 
 int main() {
 
-  char *names[] = {"apple", "mango", "ant", "banana", "cat", "anaconde"};
+  char *names[] = {"apple", "mango", "ant", "banana", "cat", "anaconda"};
   int n = sizeof(names) / sizeof(names[0]);
 
   for (int i = 0; i < n - 1; i++) {
